bfdd0a5189a1f2d35b1b1a517ad233ee40f488ee
Kamsi <kamzi247@gmail.com>
2025-12-25 13:11:26 +0100

    Added variables.py content

---
+++ variables.py
@@ -0,0 +1,9 @@
+# Variables storing integer, string and float values
+
+age= 42
+name = "Chelsea"
+weight = 70.9
+
+print(f"Her age is {age}")
+print(f"That is {name}")
+print(f"She weighs {weight}kg")
